e01bc2161638348b105f66e49f55ad5659c3ba7f
arthur_molnar <amolnar@celerity.com>
2019-04-27 14:13:23 +0300

    Create Game Board

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const gameColors = ["red","blue","green","yellow"];
 const message = document.querySelector(".message");
 const gamearea = document.querySelector(".gamearea");
 const button = document.querySelector("button");
@@ -5,3 +6,20 @@ const button = document.querySelector("button");
 button.addEventListener("click",function() {
     console.log("works");
 });
+
+window.addEventListener("load",setup);
+
+function setup() {
+    console.log("Page loaded");
+    for(let x=0;x<gameColors.length;x++) {
+        let div = eleFactory("div");
+        div.style.backgroundColor = gameColors[x];
+        div.classList.add("box");
+        gamearea.appendChild(div);
+    }
+}
+
+function eleFactory(elType) {
+    let ele = document.createElement(elType);
+    return ele;
+}
