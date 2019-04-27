731f46c7bfbdaf6096a5f7b7058b10940aea8d3d
arthur_molnar <amolnar@celerity.com>
2019-04-27 14:00:44 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const message = document.querySelector(".message");
+const gamearea = document.querySelector(".gamearea");
+const button = document.querySelector("button");
+
+button.addEventListener("click",function() {
+    console.log("works");
+});
