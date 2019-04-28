8d6dffc68076518fb4db3729e08d3a06df12269e
arthur_molnar <amolnar@celerity.com>
2019-04-28 11:46:12 +0300

    End Gameplay Check

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const button = document.querySelector("button");
 let gameClicks = [];
 let userClicks = [];
 let inPlay = false;
-let playNum = 5;
+let playNum = 2;
 
 window.addEventListener("load", setup);
 button.addEventListener("click", function () {
     if (!inPlay) {
         player();
     }
-});
+})
 
 function player() {
     button.disabled = true;
@@ -29,12 +29,12 @@ function runSequence(num) {
         inPlay = true;
         return;
     }
+
     let randomNum = Math.floor(Math.random() * gameColors.length);
     console.log(squares[randomNum]);
     gameClicks.push(gameColors[randomNum]);
     console.log(gameClicks);
     squares[randomNum].style.opacity = "1";
-
     setTimeout(function () {
         squares[randomNum].style.opacity = "0.5";
         setTimeout(function () {
@@ -64,8 +64,23 @@ function checkAnswer(e) {
         setTimeout(function () {
             el.style.opacity = "0.5";
         }, 500);
+        if(userClicks.length == gameClicks.length){
+            inPlay = false;
+            endGame();
+        }
     }
     console.log(userClicks);
+}
+
+function endGame() {
+    console.log("game over");
+    button.disabled = false;
+    if(userClicks.toString() == gameClicks.toString()) {
+        playNum++;
+        console.log("correct");
+    }else{
+        console.log("not correct");
+    }
 }
 
 function eleFactory(elType) {
