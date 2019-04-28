efc4b42f967446cbfe7a8d60abe7673fc8bb743a
arthur_molnar <amolnar@celerity.com>
2019-04-28 10:49:11 +0300

    Random Sequence of Colors

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const gameColors = ["red","blue","green","yellow"];
+const gameColors = ["red", "blue", "green", "yellow"];
 const message = document.querySelector(".message");
 const gamearea = document.querySelector(".gamearea");
 const button = document.querySelector("button");
@@ -8,10 +8,9 @@ let userClicks = [];
 let inPlay = false;
 let playNum = 1;
 
-window.addEventListener("load",setup);
-
-button.addEventListener("click",function() {
-    if(!inPlay) {
+window.addEventListener("load", setup);
+button.addEventListener("click", function () {
+    if (!inPlay) {
         player();
     }
 })
@@ -20,22 +19,35 @@ function player() {
     button.disabled = true;
     gameClicks = [];
     userClicks = [];
-    runSequence();
+    runSequence(playNum);
 }
 
-function runSequence() {
-    inPlay = true;
+function runSequence(num) {
+    let squares = document.querySelectorAll(".box");
+    num--;
+    if (num < 0) {
+        inPlay = true;
+        return;
+    }
+    let randomNum = Math.floor(Math.random() * gameColors.length);
+    console.log(squares[randomNum]);
+    gameClicks.push(gameColors[randomNum]);
+    console.log(gameClicks);
+    squares[randomNum].style.opacity = "1";
+    setTimeout(function () {
+        squares[randomNum].style.opacity = "0.5";
+    }, 500);
 }
 
 function setup() {
     console.log("Page loaded");
-    for(let x=0;x<gameColors.length;x++){
+    for (let x = 0; x < gameColors.length; x++) {
         let div = eleFactory("div");
         div.style.backgroundColor = gameColors[x];
         div.classList.add("box");
         div.style.opacity = "0.5";
         div.myColor = gameColors[x];
-        div.addEventListener("click",checkAnswer);
+        div.addEventListener("click", checkAnswer);
         gamearea.appendChild(div);
     }
 }
@@ -44,8 +56,8 @@ function checkAnswer(e) {
     if (inPlay) {
         let el = e.target;
         userClicks.push(el.myColor);
-        el.style.opacity= "1";
-        setTimeout(function() {
+        el.style.opacity = "1";
+        setTimeout(function () {
             el.style.opacity = "0.5";
         }, 500);
     }
